623a6c1bcd247bf51c6154691fe89de7d55ff975
Hasibul10884 <hasibulhasan193@gmail.com>
2022-07-17 11:58:53 +0600

    ff

--- resources/views/backend/fixed/sidebar.blade.php
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -3,9 +3,9 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <i class="fa-solid fa-user"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">OBAY Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3"> Admin </div>
     </a>
 
     <!-- Divider -->
