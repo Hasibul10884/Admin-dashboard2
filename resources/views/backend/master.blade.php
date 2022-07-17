2a9bb6f645b7a64992e4d5a027a6a691669fa744
Hasibul10884 <hasibulhasan193@gmail.com>
2022-07-17 12:04:32 +0600

    dd

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>OBAY Admin Dashboard</title>
+    <title>EBAY Admin Dashboard</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{url('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
